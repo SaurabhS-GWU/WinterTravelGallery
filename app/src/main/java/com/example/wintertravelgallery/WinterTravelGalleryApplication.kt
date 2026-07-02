7661bb2764e918dd4a235d758d239299f010ad17
Saurabh Shetty <navyashetty@Navyas-MacBook-Pro.local>
2026-07-01 23:10:08 -0700

    Initial hilt setup for project

---
+++ app/src/main/java/com/example/wintertravelgallery/WinterTravelGalleryApplication.kt
@@ -0,0 +1,7 @@
+package com.example.wintertravelgallery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WinterTravelGalleryApplication: Application()
